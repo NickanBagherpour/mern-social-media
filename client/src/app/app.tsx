afef75582726db1ade32cc63fb1fdde7ae1537f3
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2023-09-16 16:19:40 +0330

    feat: work on client's auth and navbar

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -2,27 +2,40 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "../scenes/home-page";
 import LoginPage from "../scenes/login-page";
 import ProfilePage from "../scenes/profile-page";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "../theme";
 
-function App() {
-  const isAuth = true;
+const App = () => {
+  const mode = useSelector((state: any) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state: any) => state.token));
 
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route
-            path="/home"
-            element={isAuth ? <HomePage /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/profile/:userId"
-            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
-          />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route
+              path="/"
+              element={isAuth ? <Navigate to="/home" /> : <LoginPage />}
+            />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
